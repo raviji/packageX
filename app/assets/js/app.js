4b3c016ef07f4b73dbaa7e3aefb27204cb891fc0
raviji <ravi.g@spurtreetech.com>
2016-12-22 23:17:41 +0530

    fixed one issue

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,12 +1,12 @@
 var App = angular.module("GPApp", []);
 
-App.controller("mainCtrl", function($scope) {
+App.controller("mainCtrl", function($scope,$timeout) {
 
 
     var polygon = $("svg polygon");
     
     var i = polygon.length;
-    
+   
     while (i--) {
         $(polygon).mouseenter(function(e) {
             $(e.target).parent().append(e.target);
@@ -19,70 +19,51 @@ function randomXToY(minVal,maxVal)
   return Math.round(randVal);
 }
     
-    function  toggleSomething1(){
+    function  toggleFirstSet(){
         var total = $("svg image").length;
-        //console.log($("svg image").length)
+        ////console.log($("svg image").length)
         var random = randomXToY(1,total);
-           // console.log(random)
+           // //console.log(random)
           var d = document.getElementById(random);
           var x =d.classList;
           var parentClass=$(d).parent().parent().prop('className');
-          //console.log(parentClass,random);
-          if(parentClass="ne"){
+          //console.log(parentClass);
+          if(parentClass=="ne"){
          
-         if(x==0){
-             d.classList.remove("hover1");
-         d.classList.add("hover1");
-        d.classList.remove("hover1");}
-         else{
-             d.classList.remove("hover1");
-         d.classList.remove("a");
-         d.classList.remove("hover1")
-         }
-          }
-          else{
-             // toggleSomething2();
-          }
-         //console.log(d.classList)
-          //console.log(x.length)
-      
-   
-}
-
-    function  toggleSomething2(){
-        var total = $("svg image").length-2;
-        //console.log(total)
-        var random = randomXToY(1,total);
-          var d = document.getElementById(random);
-          var parentClass=$(d).parent().parent().prop('className');
-          //console.log(parentClass,random);
-         
-          if(parentClass=="ns"){
-         var x =d.classList;
+            if(x==0)
+            {         
+               d.classList.add("hover1"); 
+                $timeout(function(){
+                    d.classList.remove("hover1"); 
+                    console.log("flip")       
+                },2000);
+            }         
+        }else if(parentClass=="ns"){
         
          if(x==0){
-         d.classList.add("hover2");}
-         else{
-         d.classList.remove("a");
-         d.classList.remove("hover2")
-         }
-          }
-           else{
-             // toggleSomething1();
-          }
-         //console.log(d.classList)
-          //console.log(x.length)
+              
+         d.classList.add("hover2");
+        $timeout(function(){
+                d.classList.remove("hover2")
+            }, 2000);
       
-    
-}
+         }
+    }
+    };
+   
+
+
+
 var check;
 var id;
 function startstop(check){
        if(!check){
       id=  window.setInterval(function(){
-  toggleSomething1();
-            toggleSomething2();
-},1000);      
+           
+  toggleFirstSet();
+  console.log("interval")
+  
+},2000);      
           }
           else{
                clearInterval(id);
@@ -92,7 +73,7 @@ function startstop(check){
                    arr.classList.remove("hover1","hover2");
                }
                
-              console.log("stopped");
+              //console.log("stopped");
           }
 
 }
@@ -107,8 +88,8 @@ function startstop(check){
     $(".box").mouseleave(function(){
         
        check=false;
-        console.log(check);
-        console.log("left");
+        //console.log(check);
+        //console.log("left");
         var count=0;
         startstop(check);
           
